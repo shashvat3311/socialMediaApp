06627ddc75eaf00c122cd527b3e1bb6637f71dd9
Shashvat Sharma <shahsvat275@gmail.com>
2024-04-06 16:09:09 +0530

    Adding all user post API's

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,113 +1,363 @@
+const db = require("../db");
+const jwt = require("jsonwebtoken");
+const validator = require("../validator");
+require("dotenv").config();
 
-const db=require('../db')
-const jwt=require('jsonwebtoken')
-require('dotenv').config();
+const createUser = async (req, res) => {
+  try {
+    const {
+      user_id,
+      firstName,
+      middleName,
+      lastName,
+      userName,
+      mobile,
+      email,
+      passwordHash,
+      intro,
+      profile,
+    } = req.body;
 
-const createUser=async(req,res)=>{
-    try{
-       const{user_id,firstName,middleName,lastName,userName,mobile,email,passwordHash,intro,profile}=req.body 
+    const queryString =
+      "insert into user(user_id,firstName,middleName,lastName,userName,mobile,email,passwordHash,intro,profile) values (?,?,?,?,?,?,?,?,?,?)";
 
-       const queryString="insert into user(user_id,firstName,middleName,lastName,userName,mobile,email,passwordHash,intro,profile) values (?,?,?,?,?,?,?,?,?,?)"
+    const quertyStringGetUser = "select * from user where user_id = ?";
 
-       const quertyStringGetUser='select * from user where user_id = ?'
+    const duplicateEnrtyQuery =
+      "select exists(select 1 from user where user_id=?) as user_id, exists(select 1 from user where username=?) as username, exists(select 1 from user where email=?) as email ,exists (select 1 from user where mobile =?) as mobile";
 
-       const duplicateEnrtyQuery='select exists(select 1 from user where user_id=?) as user_id, exists(select 1 from user where username=?) as username, exists(select 1 from user where email=?) as email ,exists (select 1 from user where mobile =?) as mobile'
+    const insertTokenQuery = "update user set user_token=? where user_id =?";
 
-       const insertTokenQuery='update user set user_token=? where user_id =?'
-
-
-    db.query(duplicateEnrtyQuery,[user_id,userName,email,mobile],(err,result)=>{   
-       if(result[0].user_id||result[0].username==1||result[0].email==1||result[0].mobile==1){
-        return res.status(409).send({
-            success:false,
-            err_message:"Duplicate entries count->",
-            duplicate:result
-        })
-       }
-       else{
-        db.query(queryString,[user_id,firstName,middleName,lastName,userName,mobile,email,passwordHash,intro,profile],(err,result)=>{
-            if(!user_id||!firstName||!lastName||!userName||!mobile||!email||!passwordHash||!intro||!profile){
+    db.query(
+      duplicateEnrtyQuery,
+      [user_id, userName, email, mobile],
+      (err, result) => {
+        if (
+          result[0].user_id ||
+          result[0].username == 1 ||
+          result[0].email == 1 ||
+          result[0].mobile == 1
+        ) {
+          return res.status(409).send({
+            success: false,
+            err_message: "Duplicate entries count->",
+            duplicate: result,
+          });
+        } else {
+          db.query(
+            queryString,
+            [
+              user_id,
+              firstName,
+              middleName,
+              lastName,
+              userName,
+              mobile,
+              email,
+              passwordHash,
+              intro,
+              profile,
+            ],
+            (err, result) => {
+              if (
+                !user_id ||
+                !firstName ||
+                !lastName ||
+                !userName ||
+                !mobile ||
+                !email ||
+                !passwordHash ||
+                !intro ||
+                !profile
+              ) {
                 return res.status(404).send({
-                    succss:false,
-                    err_message:"Please enter values for the mentioned feilds(user_id,firstName,lastName,userName,mobile,email,passwordHash,,intro,profile)"
-                })
-            }
-            if(err){
-                console.log(err)
+                  succss: false,
+                  err_message:
+                    "Please enter values for the mentioned feilds(user_id,firstName,lastName,userName,mobile,email,passwordHash,,intro,profile)",
+                });
+              }
+              if (err) {
+                console.log(err);
                 return res.status(400).send({
-                    success:false,
-                    err:err
-                })
-            }
-            db.query(quertyStringGetUser,[user_id],(err,userresult)=>{
-                if(err){
-                    return res.status(400).send({
-                        success:false,
-                        err:err
-                    })
+                  success: false,
+                  err: err.message,
+                });
+              }
+              db.query(quertyStringGetUser, [user_id], (err, userresult) => {
+                if (err) {
+                  return res.status(400).send({
+                    success: false,
+                    err: err.message,
+                  });
                 }
                 // db.query('')
-                const token = jwt.sign({ data: userresult }, process.env.TOKEN_SECRET)
-                db.query(insertTokenQuery,[token,user_id],(err,result)=>{
-                    if(err){
-                        res.status(400).send({
-                            success:false,
-                            err:err
-                        })
-                    }
-                    else{
+                const token = jwt.sign(
+                  { data: userresult },
+                  process.env.TOKEN_SECRET
+                );
+                db.query(insertTokenQuery, [token, user_id], (err, result) => {
+                  if (err) {
+                    res.status(400).send({
+                      success: false,
+                      err: err.message,
+                    });
+                  } else {
                     return res.status(200).send({
-                        success:true,
-                        message:"New user created ",
-                        result:userresult,
-                        token:token
-                    })
-                }
-                })         
-            })       
-           })
-       }
-        
-    })   
+                      success: true,
+                      message: "New user created ",
+                      result: userresult,
+                      token: token,
+                    });
+                  }
+                });
+              });
+            }
+          );
+        }
+      }
+    );
+  } catch (err) {
+    return res.status(400).send({
+      succss: false,
+      err: err.message,
+    });
+  }
+};
+
+const demoget = async (req, res) => {
+  const str = req.originalUrl;
+  console.log(str);
+
+  const { username, email, mobile } = req.body;
+  const duplicateEnrtyQuery =
+    "select exists(select 1 from user where username=?) as username, exists(select 1 from user where email=?) as email ,exists (select 1 from user where mobile =?) as mobile";
+  db.query(duplicateEnrtyQuery, [username, email, mobile], (err, result) => {
+    if (err) {
+      return res.status(400).send({
+        success: false,
+        err: err.message,
+      });
     }
-    catch(err){
+
+    if (
+      result[0].usernameExists == 1 ||
+      result[0].emailExist == 1 ||
+      result[0].mobileExists == 1
+    ) {
+      return res.status(409).send({
+        success: false,
+        err_message: "Duplicate entries count->",
+        duplicate: result,
+      });
+    }
+    return res.status(200).send({
+      success: true,
+      result: result,
+    });
+  });
+};
+
+const acidAPI = async (req, res) => {};
+
+const user_post = async (req, res) => {
+  try {
+    const { user_id, sender_id, message } = req.body;
+    if (!user_id || !sender_id || !message) {
+      return res.status(404).send({
+        success: false,
+        err: "Please enter user_id ,sender_id and message",
+      });
+    }
+    const queryString =
+      "insert into user_post(user_id,sender_id,message) values(?,?,?)";
+    db.query(queryString, [user_id, sender_id, message], (err, result) => {
+      if (err) {
         return res.status(400).send({
-            succss:false,
-            err:err
+          success: false,
+          err: err.message,
+        });
+      }
+      return res.status(200).send({
+        success: true,
+        success_message: `Post generated for user_id: ${user_id} and sender_id: ${sender_id}`,
+        result: result,
+      });
+    });
+  } catch (err) {
+    if (err) {
+      res.status(400).send({
+        success: false,
+        err: err.message,
+      });
+    }
+  }
+};
+
+const user_friend = async (req, res) => {
+  try {
+    const { sourceId, targetId, type, notes } = req.body;
+    if (!sourceId || !targetId || !type || !notes) {
+      return res.status(404).send({
+        success: false,
+        err: "Please Enter sourceId,targetId ,type and notes ",
+      });
+    }
+
+    const queryString =
+      "insert into user_friend(sourceId,targetId,type,notes) values(?,?,?,?)";
+
+    const isAlradyaFriendQuery =
+      "select * from user_friend where sourceId=? and targetId=?";
+
+    await db.query(
+      isAlradyaFriendQuery,
+      [sourceId, targetId],
+      (err, result) => {
+        if (err) {
+          res.status(500).send({
+            success: false,
+            err: err,
+          });
+        }
+        if (result.length > 0) {
+          return res.status(400).send({
+            success: false,
+            message: `User ${targetId} is Already a Friend`,
+          });
+        }
+
+        db.query(
+          queryString,
+          [sourceId, targetId, type, notes],
+          (err, result) => {
+            if (err) {
+              return res.status(400).send({
+                success: false,
+                err: err.message,
+              });
+            }
+            return res.status(200).send({
+              success: true,
+              success_message: `New friend created ${targetId}`,
+              result: result,
+            });
+          }
+        );
+      }
+    );
+  } catch (err) {
+    if (err) {
+      res.status(400).send({
+        success: false,
+        err: err.message,
+      });
+    }
+  }
+};
+const user_follower = async (req, res) => {
+    try {
+        const { sourceId, targetId} = req.body;
+        if (!sourceId || !targetId ) {
+          return res.status(404).send({
+            success: false,
+            err: "Please Enter sourceId and targetId",
+          });
+        }
+    
+        const queryString =
+          "insert into user_friend(sourceId,targetId) values(?,?)";
+    
+        const isAlradyaFollowerQuery =
+          "select * from user_friend where sourceId=? and targetId=?";
+    
+        await db.query(
+          isAlradyaFollowerQuery,
+          [sourceId, targetId],
+          (err, result) => {
+            if (err) {
+              res.status(500).send({
+                success: false,
+                err: err,
+              });
+            }
+            if (result.length > 0) {
+              return res.status(400).send({
+                success: false,
+                message: `User ${targetId} is Already a Follower`,
+              });
+            }
+    
+            db.query(
+              queryString,
+              [sourceId, targetId],
+              (err, result) => {
+                if (err) {
+                  return res.status(500).send({
+                    success: false,
+                    err: err.message
+                  });
+                }
+                return res.status(200).send({
+                  success: true,
+                  success_message: `New Follower:${targetId} for User:${sourceId}`,
+                  result: result,
+                });
+              }
+            );
+          }
+        );
+      } catch (err) {
+        if (err) {
+          res.status(400).send({
+            success: false,
+            err: err.message,
+          });
+        }
+      }
+};
+const user_message = async (req, res) => {
+  try {
+    const {sourceId,targetId,message} = req.body;
+    if(!sourceId||!targetId||!message){
+        return res.status(400).send({
+            success:false,
+            err:"Please Enter sourceId ,targetId and message"
         })
     }
-}
-
-const demoget=async(req,res)=>{
-    const str=req.originalUrl
-    console.log(str)
-
-    const{username,email,mobile}=req.body
-    const duplicateEnrtyQuery='select exists(select 1 from user where username=?) as username, exists(select 1 from user where email=?) as email ,exists (select 1 from user where mobile =?) as mobile'
-    db.query(duplicateEnrtyQuery,[username,email,mobile],(err,result)=>{
-
-        if(err){
+    const queryString = "insert into user_message(sourceId,targetId,message) values(?,?,?)" ;
+    db.query(queryString, [sourceId,targetId,message], (err, result) => {
+      if (err) {
+        if(err.errno==1452){
             return res.status(400).send({
-                success:false ,
-                err:err
+                success:false,
+                err:"Failed to send the message. The sender ID provided is not valid. Please check the sender ID and try again."
             })
         }
-        
-       if(result[0].usernameExists==1||result[0].emailExist==1||result[0].mobileExists==1){
-        return res.status(409).send({
-            success:false,
-            err_message:"Duplicate entries count->",
-            duplicate:result
-        })
-       }
-        return res.status(200).send({
-            success:true,
-            result:result
-        })
-    })
-}
-
-const acidAPI=async(req,res)=>{
-     
-}
-module.exports={createUser,demoget}
+        return res.status(400).send({
+          success: false,
+          err: err,
+        });
+      }
+      return res.status(200).send({
+        success: true,
+        success_message:`New Message:${message} successfilly sent by Source: ${sourceId} to Target: ${targetId}`,
+        result: result,
+      });
+    });
+  } catch (err) {
+    if (err) {
+      res.status(400).send({
+        success: false,
+        err: err.message,
+      });
+    }
+  }
+};
+module.exports = {
+  createUser,
+  demoget,
+  user_follower,
+  user_friend,
+  user_post,
+  user_message,
+};
--- routers/user.router.js
+++ routers/user.router.js
@@ -1,10 +1,19 @@
 const express=require('express');
 const router=express.Router();
 const userController=require('../controllers/user.controller')
-const db=require("../db");
+const validator=require('../validator')
 
 router.post('/createUser',userController.createUser)
 
 router.get('/demoGet',userController.demoget)
+
+router.post('/user_post',userController.user_post)
+
+router.post('/user_friend',userController.user_friend)
+
+router.post('/user_message',userController.user_message)
+
+router.post('/user_follower',userController.user_follower)
+
 
 module.exports=router
